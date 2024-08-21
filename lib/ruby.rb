c97d147c71e7ea086465202ebd24413fef6743ca
Roderick perez <roderickalbertop@gmail.com>
2024-08-21 16:03:15 -0600

    subida 3

---
+++ lib/ruby.rb
@@ -0,0 +1,15 @@
+def intro array
+    #escribir el código
+    return 0 if array.empty?
+    array.sum
+end
+def nombre nom
+    return "vacío" if nom.empty?
+    #escribir el código
+    "hello #{nom.capitalize}"
+
+end
+def datos? array
+    #escribir el código
+    array.size> 0
+end
